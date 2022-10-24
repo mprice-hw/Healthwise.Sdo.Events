5d2552724e8416b19cfc63d6030c9c497269ee76
Mike Price <mprice@healthwise.org>
2022-10-23 23:47:36 -0600

    Added required attribute to properties

--- Healthwise.Sdo.Events/AzurePipelines/PipelineExecutionEvent.cs
+++ Healthwise.Sdo.Events/AzurePipelines/PipelineExecutionEvent.cs
@@ -4,38 +4,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Converters;
+using System.ComponentModel.DataAnnotations;
 
 namespace Healthwise.Sdo.Events.AzurePipelines
 {
     public class PipelineExecutionEvent : EventBase
     {
+        [Required]
         [JsonProperty("name")]
         public string PipelineName { get; set; }
 
+        [Required]
         [JsonProperty("runId")]
         public int RunId { get; set; }
 
+        [Required]
         [JsonProperty("teamName")]
         public string TeamName { get; set; }
 
+        [Required]
         [JsonProperty("serviceName")]
         public string ServiceName { get; set; }
-
+        
+        [Required]
         [JsonProperty("serviceVersion")]
         public string ServiceVersion { get; set; }
 
+        [Required]
         [JsonProperty("repoName")]
         public string RepoName { get; set; }
 
+        [Required]
         [JsonProperty("branchName")]
         public string BranchName { get; set; }
 
+        [Required]
         [JsonProperty("commitHash")]
         public string CommitHash { get; set; }
 
+        [Required]
         [JsonProperty("state")]
         public PipelineState State { get; set; }
 
+        [Required]
         [JsonProperty("result")]
         public PipelineResult Result { get; set; }
     }
--- Healthwise.Sdo.Events/EventBase.cs
+++ Healthwise.Sdo.Events/EventBase.cs
@@ -5,23 +5,29 @@ using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Healthwise.Sdo.Events
 {
     public class EventBase
     {
+        [Required]
         [JsonProperty("source")]
         public string Source { get; set; }
 
+        [Required]
         [JsonProperty("type")]
         public string Type { get; set; }
 
+        [Required]
         [JsonProperty("version")]
         public string Version { get; set; }
-
+        
+        [Required]
         [JsonProperty("sourceId")]
         public string SourceId { get; set; }
 
+        [Required]
         [JsonProperty("timestamp")]
         [JsonConverter(typeof(IsoDateTimeConverter))]
         public DateTimeOffset Timestamp { get; set; }
